3b48dc7ee81f3ecd946ded66878cd27f40905813
superbrothers <superbrothers@shisobu.in>
2012-03-19 09:41:44 +0900

    content をxml.js内で生成することをやめる

--- test/lib/test_xml.js
+++ test/lib/test_xml.js
@@ -7,26 +7,32 @@ var path = require("path")
 
 module.exports = {
   setUp: function (callback) {
+    var res = [
+      {
+          date_str: new Date()
+        , title   : "title"
+        , link    : "http://example.jp"
+        , content : "content"
+      }
+    ];
+    this.xml = generateXML("title", res);
+
     callback();
   },
   tearDown: function (callback) {
     callback();
   },
+  "contentが正しく設定できている": function (test) {
+    var matches = this.xml.match(/<content:encoded><\!\[CDATA\[content]]><\/content:encoded>/);
+    test.notEqual(matches, null);
+    test.done();
+  },
   "xmllintが通る": function (test) {
-      var res = [
-        {
-            date_str: new Date()
-          , title   : "title"
-          , link    : "http://example.jp"
-        }
-      ]
-        , xml = generateXML("title", res, function () { return "content";});
-
-      exec("echo '" + xml + "' | xmllint --noout -",
-              function (err, stdout, stderr) {
-        test.equal(stderr, "");
-        test.done();
-      });
+    exec("echo '" + this.xml + "' | xmllint --noout -",
+            function (err, stdout, stderr) {
+      test.equal(stderr, "");
+      test.done();
+    });
   }
 };
 /* vim: set ts=2 sts=2 sw=2: */
