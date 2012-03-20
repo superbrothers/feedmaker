1685c76e4e48ff82cb583934e8977065c179cf48
superbrothers <superbrothers@shisobu.in>
2012-03-20 09:25:01 +0900

    整形

--- bin/dattorss.js
+++ bin/dattorss.js
@@ -13,25 +13,25 @@ var generateXML = require(path.join('..', 'lib', 'xml.js');
 var res = [];
 
 function parse_res (thread, uri, item) {
-    var date_str = regex(/([0-9]{4}\/[0-9]{2}\/[0-9]{2})[^0-9]+([0-9]{2}:[0-9]{2}:[0-9]{2})/g, item, {concat:'$1 $2', limit:1});
+    var date_str = regex(/([0-9]{4}\/[0-9]{2}\/[0-9]{2})[^0-9]+([0-9]{2}:[0-9]{2}:[0-9]{2})/g, item, {concat: '$1 $2', limit: 1});
     var date = new Date(date_str);
-    var mailto = regex(/<a href="mailto[^"]+"><b>([^<]+)<\/b><\/a>/g, item, {concat:'$1', limit:1});
-    var time = regex(/[0-9]{4}\/([0-9]{2}\/[0-9]{2})[^0-9]+([0-9]{2}:[0-9]{2}):[0-9]{2}/g, item, {concat:'$1 $2', limit:1});
-    var ids = regex(/ID:([^<]+)<dd>/g, item, {concat:'$1', limit:1});
-    var body = regex(/ID:[^<]+<dd>(.*?)$/g, item, {concat:'$1', limit:1});
+    var mailto = regex(/<a href="mailto[^"]+"><b>([^<]+)<\/b><\/a>/g, item, {concat: '$1', limit: 1});
+    var time = regex(/[0-9]{4}\/([0-9]{2}\/[0-9]{2})[^0-9]+([0-9]{2}:[0-9]{2}):[0-9]{2}/g, item, {concat: '$1 $2', limit: 1});
+    var ids = regex(/ID:([^<]+)<dd>/g, item, {concat: '$1', limit: 1});
+    var body = regex(/ID:[^<]+<dd>(.*?)$/g, item, {concat: '$1', limit: 1});
     var title = body.replace(/<[^>]+>/g, '').replace(/[ 　]/g, '').slice(0, 30);
     var num = +(regex(/([0-9]+)/g, item, {concat:'$1', limit:1}));
-    return {mailto:mailto, ids:ids, date:date, num:num, thread:thread, title:title, time:time, body:body, link:uri+'/'+num};
+    return {mailto: mailto, ids :ids, date: date, num: num, thread: thread, title: title, time: time, body: body, link: uri + '/' + num};
 }
 
 function attach_css (item, anc) {
     var html = "";
     if (anc === false) {
-        html += "<dt><span style=\"color:#696969\"><strong>"+item.thread+"</strong></span></dt><br />";
+        html += "<dt><span style=\"color:#696969\"><strong>" + item.thread + "</strong></span></dt><br />";
     }
-    html += "<dt><span style=\"color:#4169e1\"><strong>"+item.num+"</strong></span>: ";
-    html += "<span style=\"color:#008000\"><strong>"+item.mailto+"</strong></span>: ";
-    html += item.time+": <span style=\"color:#696969\"><strong>"+item.ids+"</strong></span>";
+    html += "<dt><span style=\"color:#4169e1\"><strong>" + item.num + "</strong></span>: ";
+    html += "<span style=\"color:#008000\"><strong>" + item.mailto + "</strong></span>: ";
+    html += item.time+": <span style=\"color:#696969\"><strong>" + item.ids + "</strong></span>";
     html += "<dd>" + item.body + "</dd>";
     if (anc === false) {
         html += item.anc_html;
@@ -60,12 +60,12 @@ function unique (array) {
 function search_anc (res_json, res_ary) {
     if (res_json.num < 2) return "";
     var anc_html = "";
-    var anc_ary = regex(/&gt;&gt;([0-9]{1,3})/g, res_json.body, {concat:'$1', limit:10});
+    var anc_ary = regex(/&gt;&gt;([0-9]{1,3})/g, res_json.body, {concat: '$1', limit: 10});
     anc_ary = unique(anc_ary);
     anc_ary.forEach(function (i) {
         if (i === '1' || +i === 1) return;
         res_ary.forEach(function (item) {
-            if (item.indexOf("<dt>"+i+" ：") !== 0) return;
+            if (item.indexOf("<dt>" + i + " ：") !== 0) return;
             var obj = parse_res("", "", item);
             var cssed = attach_css(obj, true);
             anc_html += attach_anc(cssed);
@@ -75,8 +75,8 @@ function search_anc (res_json, res_ary) {
 }
 
 var queue = async.queue(function (task, callback) {
-    scrape.request({uri:task.uri, encoding:'sjis'}, function (err, status, data) {
-        var thread = regex(/<title>([^<]+)<\/title>/g, data, {concat:'$1', limit:1});
+    scrape.request({uri: task.uri, encoding: 'sjis'}, function (err, status, data) {
+        var thread = regex(/<title>([^<]+)<\/title>/g, data, {concat: '$1', limit: 1});
         var res_ary = data.split('\n');
         if (!res_ary) res_ary = [];
         res_ary.forEach(function (item) {
@@ -95,9 +95,9 @@ var queue = async.queue(function (task, callback) {
 }, 1);
 
 function zP(val) {
-    vS = val.toString(16) ;
+    var vS = val.toString(16) ;
     tmpS = ("00" + vS).slice(-2) ;
-    return tmpS ;
+    return tmpS;
 }
 
 queue.drain = function() {
@@ -106,9 +106,9 @@ queue.drain = function() {
     console.log(xml);
 }
 
-process.argv.splice(3, process.argv.length-3).forEach(function (uri) {
+process.argv.splice(3, process.argv.length - 3).forEach(function (uri) {
     console.error(uri);
-    queue.push({uri:uri}, function (err) {
+    queue.push({uri: uri}, function (err) {
         return;
     });
 });
